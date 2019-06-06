ea41e9a7094668d7e8d5b84f672398bb8b214493
Michelle Wagner <mewag13@yahoo.com>
2019-06-06 04:24:18 +0000

    Compute correct change up to 0.00

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //costInt = cost as Integer
 //cashInt = cash as Integer
 
+
+
+
 function correctChange(cost, cashProvided) {
     
 var costInt = cost * 100;
@@ -9,9 +12,45 @@ var cashInt = cashProvided * 100;
 var change = {
     pennies: 0,
     nickels: 0,
+    dimes: 0,
+    quarters: 0,
+    dollars: 0,
+    fives: 0,
+    twenties: 0,
 };
 
 var changeRemaining = cashInt - costInt;
+
+if (changeRemaining >= 2000) {
+    change.twenties = Math.floor(changeRemaining / 2000);
+    changeRemaining =  changeRemaining % 2000;
+    
+}
+
+if (changeRemaining >= 500) {
+    change.fives = Math.floor(changeRemaining / 500);
+    changeRemaining =  changeRemaining % 500;
+    
+}
+
+
+if (changeRemaining >= 100) {
+    change.dollars = Math.floor(changeRemaining / 100);
+    changeRemaining =  changeRemaining % 100;
+}
+ 
+
+if (changeRemaining >= 25) {
+    change.quarters = Math.floor(changeRemaining / 25);
+    changeRemaining =  changeRemaining % 25;
+    
+}
+
+if (changeRemaining >= 10) {
+    change.dimes = Math.floor(changeRemaining / 10);
+    changeRemaining =  changeRemaining % 10;
+    
+}
 
 if (changeRemaining >= 5) {
     change.nickels = 1;
@@ -19,7 +58,7 @@ if (changeRemaining >= 5) {
     
 }
 
-change.pennies = changeRemaining; 
+change.pennies = Math.round(changeRemaining); 
 
 return change;
    
--- index.test.js
+++ index.test.js
@@ -8,6 +8,11 @@ beforeEach(() => {
   expected = {
       pennies: 0,
       nickels: 0,
+      dimes: 0,
+      quarters: 0,
+      dollars: 0,
+      fives: 0,
+      twenties: 0,
   };
 });
 
@@ -29,14 +34,99 @@ test ('Provides one cent as change when the customer paid with one cent more tha
  test ('Provides a nickel as change when customer paid with 5 cents more than required', () => { 
     var change = correctChange(.95,1.00);
     expected.nickels = 1;
-   expect(change).toEqual(expected);
+    expect(change).toEqual(expected);
  });
  
   test ('Provides a nickel and penny as change when customer paid with 6 cents more than required', () => { 
     var change = correctChange(.94,1.00);
      expected.pennies = 1;
      expected.nickels = 1;
-   expect(change).toEqual(expected);
+     expect(change).toEqual(expected);
  });
+
+ test ('Provides a dime as change when customer paid with 10 cents more than required', () => { 
+    var change = correctChange(1.00,1.10);
+     expected.dimes = 1;
+     expect(change).toEqual(expected);
+ });
+ 
+  test ('Provides a dime, a nickel, and a penny as change when customer paid with 16 cents more than required', () => { 
+    var change = correctChange(1.00,1.16);
+     expected.dimes = 1;
+     expected.nickels = 1;
+     expected.pennies = 1;
+     expect(change).toEqual(expected);
+ });
+  test ('Provides a two dimes as change when customer paid with 20 cents more than required', () => { 
+    var change = correctChange(1.00,1.20);
+     expected.dimes = 2;
+     expect(change).toEqual(expected);
+ });
+ 
+  test ('Provides a two dimes and four pennies as change when customer paid with 24 cents more than required', () => { 
+    var change = correctChange(1.00,1.24);
+    expected.dimes = 2;
+    expected.pennies = 4;
+    expect(change).toEqual(expected);
+ });
+ 
+ test ('Provides a quarter as change when customer paid with 25 cents more than required', () => { 
+    var change = correctChange(19.75, 20.00);
+    expected.quarters = 1;
+    expect(change).toEqual(expected);
+ });
+ 
+ test ('Provides a quarter, two dimes. and four pennies as change when customer paid with 49 cents more than required', () => { 
+    var change = correctChange(19.51, 20.00);
+    expected.quarters = 1;
+    expected.dimes = 2;
+    expected.pennies = 4;
+    expect(change).toEqual(expected);
+ });
+ 
+ test ('Provides a one dollar as change when customer paid with 100 cents more than required', () => { 
+    var change = correctChange(19.00, 20.00);
+    expected.dollars = 1;
+    expect(change).toEqual(expected);
+ });
+ 
+  test ('Provides a five dollar as change when customer paid with 100 cents more than required', () => { 
+    var change = correctChange(20.00, 25.00);
+    expected.fives = 1;
+    expect(change).toEqual(expected);
+ });
+ 
+  test ('Provides a five, four ones, three quarters, two dimes, and four pennies as change when customer paid with 999 cents more than required', () => { 
+    var change = correctChange(20.00, 29.99);
+    expected.fives = 1;
+    expected. dollars = 4;
+    expected.quarters = 3;
+    expected.dimes = 2;
+    expected.pennies = 4;
+    expect(change).toEqual(expected);
+ });
+ 
+  test ('Provides a twenty dollar bill as change when customer paid with 2000 cents more than required', () => { 
+    var change = correctChange(30.00, 50.00);
+    expected.twenties = 1;
+    expect(change).toEqual(expected);
+ });
+ 
+ 
+ 
+
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
 
