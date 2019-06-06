b33464bc023799fb0c2fa075cc1d8f755ba2e6f8
Michelle Wagner <mewag13@yahoo.com>
2019-06-06 01:54:50 +0000

    Returns correct pennies

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function correctChange() {
+function correctChange(cost, cashProvided) {
+    
+var costAsInterger = cost * 100;
+var cashAsInterger = cashProvided * 100;
 
+var pennies = cashAsInteger - costAsInteger;
+
+return {
+    pennies: pennies,
+  };
 }
 
 module.exports.correctChange = correctChange;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,19 @@
-const index = require('./index');
+var index = require('./index');
+
+var correctChange = index.correctChange;
 
 test('correctChange exists',() => {
-  index.correctChange();
+  correctChange();
 });
+
+test ('Provides no change when the customer provided correct change', () => {
+    var change = correctChange(1.00,1.00);
+    expect(change).toEqual({ pennies: 0});
+ });
+
+test ('Provides one cent as change when the customer paid with one cent more than required', () => { 
+    var change = correctChange(.99,1.00);
+    expect(change).toEqual({ pennies: 1});
+ });
+
+
